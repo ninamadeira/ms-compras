561a871109038e232de43fe8b0c6a246df340e7c
Nina Rosa Madeira <nina.madeira@gmail.com>
2022-02-22 19:50:13 -0300

    Alteração do workercompras

--- workercompras/src/main/java/com/workercompras/workercompras/WorkercomprasApplication.java
+++ workercompras/src/main/java/com/workercompras/workercompras/WorkercomprasApplication.java
@@ -3,7 +3,6 @@ package com.workercompras.workercompras;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableRabbit
 @SpringBootApplication
