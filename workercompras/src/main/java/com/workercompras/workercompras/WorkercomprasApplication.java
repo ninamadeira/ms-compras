f6a73cde437aad2c51bbef3f39a00be75286970e
ninamadeira <nina.madeira@gmail.com>
2022-02-22 19:23:42 -0300

    Alterações no workercompras

--- workercompras/src/main/java/com/workercompras/workercompras/WorkercomprasApplication.java
+++ workercompras/src/main/java/com/workercompras/workercompras/WorkercomprasApplication.java
@@ -5,14 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @EnableRabbit
 @SpringBootApplication
 public class WorkercomprasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkercomprasApplication.class, args);
-		//SpringApplication.run(WorkercomprasApplication.class, "--debug");
 	}
 
 }
